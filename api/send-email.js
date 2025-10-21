a962f5d4bac6256d225f92adefbe70c7d4acbaf1
jasdigital <jake@jasdigital.co.uk>
2025-10-21 22:50:01 +0100

    Update mailer ocnfiguration

--- api/send-email.js
+++ api/send-email.js
@@ -1,5 +1,5 @@
 // SendGrid Mail API for Vercel deployment
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
